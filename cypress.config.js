c662bb36a12eb86f1c749e8eb33128452854638e
LALIT KUSHWAHA <45065608+lalitkushwaha1410@users.noreply.github.com>
2022-07-30 23:46:43 +0530

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'ca453y',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
