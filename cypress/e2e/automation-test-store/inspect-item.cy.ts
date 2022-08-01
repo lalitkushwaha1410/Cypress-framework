f76b6ed5e90435103f3e0e9de820623f450e14a2
LALIT KUSHWAHA <45065608+lalitkushwaha1410@users.noreply.github.com>
2022-08-02 00:42:49 +0530

    added new tc

--- cypress/e2e/automation-test-store/inspect-item.cy.ts
+++ cypress/e2e/automation-test-store/inspect-item.cy.ts
@@ -9,9 +9,14 @@ describe('inspect item', () => {
       cy.get('a[title="Skinsheen Bronzer Stick"]').scrollIntoView().click();
     })
 
-    it('test with chaining commands', () => {
+    it('test2 - test with chaining commands', () => {
 
       cy.get('.prdocutname').contains('Skinsheen Bronzer Stick').scrollIntoView().click();
+    })
+
+    it.only('test3- test with chaining commands ', () => {
+
+      cy.get('.fixed_wrapper').get('.prdocutname').eq(0).click();
     })
 
   })
