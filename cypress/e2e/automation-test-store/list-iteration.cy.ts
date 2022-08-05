f9b0d062dc3bfce0c30f32bd0828a7db188e35ef
LALIT KUSHWAHA <45065608+lalitkushwaha1410@users.noreply.github.com>
2022-08-05 15:56:21 +0530

    iterate over elements

---
+++ cypress/e2e/automation-test-store/list-iteration.cy.ts
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe('iterate over list', () => {
+    
+
+    it('click on Men From header', () => {
+    cy.visit('https://automationteststore.com/')
+    cy.get('a[href="https://automationteststore.com/index.php?rt=product/category&path=58"]').click();
+    cy.get('li[class="col-md-2 col-sm-2 col-xs-6 align_center"]').each(($el, index, $list) => {
+
+        cy.log($el.text());
+
+        if ($el.text().includes('Skincare')) {
+            cy.wrap($el).click();
+        }
+      })
+    })
+  })
